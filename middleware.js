96433783614681d52721c92c73b604fb3ec5d026
Dmitry Leshchinskiy <polegraf@gmail.com>
2026-09-26 14:49:23 +0100

    Fix middleware duplicate

--- middleware.js
+++ middleware.js
@@ -7,12 +7,3 @@ export function middleware(request) {
 export const config = {
   matcher: ['/api/:path*'],
 };
-import { NextResponse } from 'next/server';
-
-export function middleware(request) {
-  return NextResponse.next();
-}
-
-export const config = {
-  matcher: ['/api/:path*'],  // Только для API, не трогаем прото
-};
